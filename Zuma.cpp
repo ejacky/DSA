1683481168489b6470936003311585dccc2cf7df
ejacky <ejacky001@gmail.com>
2014-10-27 18:47:22 +0800

    dev zuma.cpp

--- Zuma.cpp
+++ Zuma.cpp
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <iostream>
+#pragma warning(disable:4996)
 #define Length 10050
 
 using   namespace   std;
-int str[Length];
+char str[Length];
+int count;
 
 bool checkColor(char lr, char color) {
 
@@ -12,10 +14,11 @@ bool checkColor(char lr, char color) {
 }
 
 void insert(int n, int k, char ch) {
-	for (int i = n; i > k; i--) {
-		str[n] = str[n - 1];
+	for (int i = n; i >= k; i--) {
 		if (i == k) {
-			str[i] = ch;
+			str[k] = ch;
+		} else {
+			str[i] = str[i - 1];
 		}
 	}
 }
@@ -31,7 +34,7 @@ bool checkrDel(int k) {
 void rDel(int n, int k) {
 	if (str[k] == str[k + 1] && str[k] == str[k + 2]) {
 		for (int i = k; i <= n - 3; i++) {
-			str[k] = str[k + 3];
+			str[i] = str[i + 3];
 		}
 	}
 }
@@ -44,8 +47,8 @@ bool checklDel(int k) {
 }
 
 void lDel(int n, int k) {
-	for (int i = k; i <= n - 3; i++) {
-			str[k - 2] = str[k + 1];
+	for (int i = k; i <= n - 1; i++) {
+			str[i - 2] = str[i + 1];
 		}
 }
 
@@ -60,13 +63,13 @@ bool checkcDel(int k) {
 void cDel(int n, int k) {
 	if (str[k] == str[k - 1] && str[k] == str[k + 1]) {
 		for (int i = k; i <= n - 3; i++) {
-			str[k - 1] = str[k + 2];
+			str[i - 1] = str[i + 2];
 		}
 	}
 }
 
 void show(int n) {
-	if (str[0] <= 'A' || str[0] >= 'Z') {
+	if (str[0] < 'A' || str[0] > 'Z') {
 		printf("-\n");
 	}
 	for (int i = 0; i < n; i++) {
@@ -79,29 +82,41 @@ void show(int n) {
 	printf("\n");
 }
 
-int del(int n, int k) {
+void del(int n, int k) {
 	int count = 0;
 	while (k >= 0 && n >= 0) {
-		if (!(checkrDel(k) && checklDel(k))) {
+		if (checkrDel(k) && checklDel(k)) {
+			printf("dont del:\n");
 			break;
 		}
 
 		if (checkrDel(k)) {
+			printf("right del:\n");
 			rDel(n, k);	
+			n = n - 3;
+			k = k - 1;
+			show(n);
 			count++;
+			printf("count:%d \n", count);
 		}
 
 		if (checkrDel(k)) {
+			printf("left del:\n");
 			lDel(n, k);
+			n = n - 3;
+			k = k - 1;
+			show(n);
 			count++;
+			printf("count:%d \n", count);
 		}
+		
 
-		del(n - 3, k - 1);
-		k--;
+		del(n, k);
+		printf("n:%d, k:%d", n, k);
 	}
 }
 
-void handleStr(int n, int k, char ch) {
+void handleStr(int n, int k) {
 	if (checkcDel(k)) {
 		cDel(n, k);
 		del(n - 3, k - 2);
@@ -113,6 +128,7 @@ void handleStr(int n, int k, char ch) {
 
 int main()
 {
+	int count;
 	for (int i = 0; i < Length; i++) {
 		char c;
 		scanf("%c", &c);
@@ -121,21 +137,53 @@ int main()
 			str[i] = c;
 		}
 		else {
+		    count = i;
 			break;
 		}
-
-		int n;
-		scanf("%d", &n);
-
-		int k;
-		char ch;
-
-		for (int i = 0; i < n; i++) {
-			scanf("%d %c", &k, &ch);
-
-			handleStr(n, k, ch);
-			show(n);
-		}
 	}
+	printf("have %d chars\n", count);
+	show(count);
+	printf("\n=====\n");
+	printf("insert:\n\n");
+
+	int k;
+	char ch;
+	scanf("%d %c", &k, &ch);
+	printf("insert %c in %d\n", ch, k);
+	insert(count, k, ch);
+	count++;
+	show(count);
+
+	printf("\n==========\n");
+	printf("zuma begin:\n");
+	printf("del: \n");
+	//handleStr(count, k);
+	//rDel(count, k);
+	//lDel(count, k);
+	del(count, k);
+	show(count);
+
+	//int n;
+	//scanf("%d", &n);
+
+	//int k;
+	//char ch;
+	//printf("%d %d %d,", sizeof(str), sizeof(str[0]), sizeof(str) / sizeof(*str));
+	//scanf("%d %c", &k, &ch);
+	//show(n);
+
+	//for (int i = 0; i < n; i++) {
+	//	scanf("%d %c", &k, &ch);
+	//	insert(n, k, ch);
+	//	show(n);
+
+	//	handleStr(n, k, ch);
+	//	show(n);
+	//}
+	
+
+
+
+	system("PAUSE");
 }
 
